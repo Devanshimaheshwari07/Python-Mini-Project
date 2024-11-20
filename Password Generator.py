103523f07ee604249118cdd2063783b29378588f
Devanshimaheshwari07 <devanshimaheshwari07@gmail.com>
2024-11-20 23:22:12 +0530

    Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,59 +1,50 @@
 import random
 import string
 
-alphabets = string.ascii_letters
-digits = string.digits
-specialCharacters = string.punctuation
+class PasswordGenerator:
+    def __init__(self, length):
+        self.length = length
+        self.alphabets = string.ascii_letters
+        self.digits = string.digits
+        self.special_characters = string.punctuation
+        self.password_characters = list(self.alphabets + self.digits + self.special_characters)
 
-passwordChara = list(alphabets + digits + specialCharacters)
+    def generate_random_password(self):
+        """Generate a password with random characters."""
+        return "".join(random.sample(self.password_characters, self.length))
 
-def condition_random_password():
-  length = input("Enter the Password Length: ")
-  if int(length.isdigit()):
-     alpha_count = input("Enter the Alphabets Count in Password: " )
-     if int(alpha_count.isdigit()):
-        digits_count = input("Enter the Digits Count in Password: ")
-        if int(digits_count.isdigit()):
-            special_count = input("Enter the Special Characters Count: ")
-            if int(special_count.isdigit()):
-                    characters_count = int(alpha_count) + int(digits_count) + int(special_count)
-                    if characters_count > int(length):
-                        print("The entered Character Count is Greater than the total Password Length")
-                        return
-                    password = [] 
-                    for i in range(int(alpha_count)):
-                        password.append(random.choice(alphabets))
-                    for i in range(int(digits_count)):
-                        password.append(random.choice(digits))
-                    for i in range(int(special_count)):
-                        password.append(random.choice(specialCharacters))
-                    if characters_count < int(length):
-                        random.shuffle(passwordChara)
-                        for i in range(int(length) - characters_count):
-                            password.append(random.choice(passwordChara))
-                    print("".join(password))
-            else:
-              print("Enter a valid special count")
-        else:
-          print("Enter a valid digit count")
-     else: 
-       print('Enter a valid alpha count')
-  else: 
-    print("Enter a valid length")
+    def generate_password_with_conditions(self, alpha_count, digit_count, special_count):
+        """Generate a password based on specified counts."""
+        total_count = alpha_count + digit_count + special_count
+        if total_count > self.length:
+            raise ValueError("Total character count exceeds password length.")
+        password = []
+        password.extend(random.choices(self.alphabets, k=alpha_count))
+        password.extend(random.choices(self.digits, k=digit_count))
+        password.extend(random.choices(self.special_characters, k=special_count))
+        if total_count < self.length:
+            password.extend(random.choices(self.password_characters, k=self.length - total_count))
+        random.shuffle(password)
+        return "".join(password)
 
-def random_password():
-  singlePassword = string.ascii_letters + string.digits + string.punctuation
-  length = input("Enter the Password Length: ")
-  if length.isdigit():
-    print("".join(random.sample(singlePassword, int(length))))
-  else:
-    print("Please enter a valid length")
+# Example Usage
+try:
+    print("1: Generate password with conditions")
+    print("2: Generate random password")
+    option = input("Choose an option: ")
+    length = int(input("Enter password length: "))
 
-print("Please Select the below options \n ")
-option = input("1 - Generating based on Conditions \n2 - Randomly Generating the Password \n Enter the Option: ")
-if option == "1": 
-  condition_random_password()
-elif option == "2":
-    random_password()
-else:
-  print("Invalid Option")
+    generator = PasswordGenerator(length)
+
+    if option == "1":
+        alpha = int(input("Enter the number of alphabets: "))
+        digits = int(input("Enter the number of digits: "))
+        special = int(input("Enter the number of special characters: "))
+        print("Generated Password:", generator.generate_password_with_conditions(alpha, digits, special))
+    elif option == "2":
+        print("Generated Password:", generator.generate_random_password())
+    else:
+        print("Invalid option selected.")
+except ValueError as e:
+    print("Error:", e)
+
